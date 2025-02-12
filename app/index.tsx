31b023847c5e26fc3a88cf9a1e37233ae47efdbb
Gideon505 <gideonshobowale@yahoo.co.uk>
2025-02-12 18:15:46 +0000

    index changes

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Hello World! i am a cat.</Text>
     </View>
   );
 }
