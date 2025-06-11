c5057cbfa221dd211aa5557f77f413b3a8fb3934
Gideon505 <gideonshobowale@yahoo.co.uk>
2025-06-11 18:49:13 +0100

    fix merge

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,24 @@
 module.exports = {
   root: true,
-  extends: ["expo"],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  plugins: ['react', '@typescript-eslint'],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'expo', // only if using Expo
+  ],
+  rules: {
+    'react/jsx-no-undef': 'error',
+    'react/react-in-jsx-scope': 'off', // <-- disable this rule
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
